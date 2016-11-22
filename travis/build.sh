b33c3a8b45387b28d0cc601ac1d558d93a42debd
Bartosz Taczała <bartosz.taczala@mobica.com>
2016-11-22 12:01:54 +0100

    Adding (working!) travis integration

--- travis/build.sh
+++ travis/build.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -e #Exit on first error
-
-#Configure, build and test
+source /opt/qt56/bin/qt56-env.sh
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
