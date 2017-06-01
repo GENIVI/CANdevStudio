c66542cca9084f1aed85c87297d940f3dabc251e
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2017-06-01 08:43:26 +0200

    integration of Travis, AppVeyor and CodeCov
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- tests/example.cpp
+++ tests/example.cpp
@@ -1,7 +1,11 @@
 #define CATCH_CONFIG_RUNNER
 #include <fakeit.hpp>
+#include "candevice/candevice.h"
 
 TEST_CASE("Simple test", "[example]") {
+    CanDevice canDevice;
+    canDevice.init("socketcan", "can0");
+    canDevice.start();
 }
 
 int main(int argc, char *argv[])
