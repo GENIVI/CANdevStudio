b9218b63d465b0745341fdebb8b173351d5ec644
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2017-06-05 10:12:46 +0200

    Forwarding CodeCov token to docker
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- .travis.sh
+++ .travis.sh
@@ -7,9 +7,13 @@ if [ "$WITH_COVERAGE" == "ON" ]; then
 fi
 
 echo "CMake: $cmake_command"
-docker run -e WITH_COVERAGE=$WITH_COVERAGE -e CXX=$CXX -v `pwd`:/root/sources bartekt/can_dev_base_image /bin/bash -c \
-     "cd /root/sources && mkdir -p build && cd build && source /opt/qt58/bin/qt58-env.sh &&
-      $cmake_command && 
-      make -j5 &&
-      make test && 
-      if [ '$WITH_COVERAGE' == 'ON' ]; then bash <(curl -s https://codecov.io/bash) -x gcov-6 || echo 'Codecov did not collect coverage reports'; fi"
+docker run
+     -e CODECOV_TOKEN=$CODECOV_TOKEN
+     -e WITH_COVERAGE=$WITH_COVERAGE
+     -e CC=$CC -e CXX=$CXX
+     -v `pwd`:/root/sources bartekt/can_dev_base_image /bin/bash
+     -c "cd /root/sources && mkdir -p build && cd build && source /opt/qt58/bin/qt58-env.sh &&
+        $cmake_command &&
+        make -j5 &&
+        make test &&
+        if [ '$WITH_COVERAGE' == 'ON' ]; then bash <(curl -s https://codecov.io/bash) -x gcov-6 || echo 'Codecov did not collect coverage reports'; fi"
