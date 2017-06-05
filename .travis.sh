4459555559d4cc96698fe5d08cdf03adcf71af72
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2017-06-05 09:24:13 +0200

    Move CodeCov to docker
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- .travis.sh
+++ .travis.sh
@@ -7,12 +7,9 @@ if [ "$WITH_COVERAGE" == "ON" ]; then
 fi
 
 echo "CMake: $cmake_command"
-docker run -e CXX=$CXX -v `pwd`:/root/sources bartekt/can_dev_base_image /bin/bash -c \
-     "mkdir -p /root/build && cd /root/build && source /opt/qt58/bin/qt58-env.sh &&
+docker run -e WITH_COVERAGE=$WITH_COVERAGE -e CXX=$CXX -v `pwd`:/root/sources bartekt/can_dev_base_image /bin/bash -c \
+     "rm -rf /root/build && mkdir -p /root/build && cd /root/build && source /opt/qt58/bin/qt58-env.sh &&
       $cmake_command && 
       make -j5 &&
-      make test"
-
-if [ "$WITH_COVERAGE" == "ON" ]; then
-    bash <(curl -s https://codecov.io/bash) -x gcov-6 || echo "Codecov did not collect coverage reports"
-fi
+      make test && 
+      if [ '$WITH_COVERAGE' == 'ON' ]; then bash <(curl -s https://codecov.io/bash) -x gcov-6 || echo 'Codecov did not collect coverage reports'; fi"
