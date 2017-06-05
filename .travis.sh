100c2d119085034cf719df729ae8082b8eed7650
Remigiusz Kołłątaj <remigiusz.kollataj@gmail.com>
2017-06-05 12:42:53 +0200

    Ci integration (#2)
    Integration of Travis, AppVeyor and CodeCov

---
+++ .travis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cmake_command="cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE"
+
+if [ "$WITH_COVERAGE" == "ON" ]; then
+    cmake_command="$cmake_command -DWITH_COVERAGE=ON"
+fi
+
+echo "CMake: $cmake_command"
+echo "ci_env: $ci_env"
+docker run \
+     $ci_env -e WITH_COVERAGE -e CC -e CXX \
+     -v `pwd`:/root/sources bartekt/can_dev_base_image /bin/bash \
+     -c "cd /root/sources && rm -rf build && mkdir -p build && cd build && source /opt/qt58/bin/qt58-env.sh &&
+        $cmake_command &&
+        make -j5 &&
+        make test &&
+        if [ '$WITH_COVERAGE' == 'ON' ]; then bash <(curl -s https://codecov.io/bash) -x gcov-6 || echo 'Codecov did not collect coverage reports'; fi"
--- travis/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /opt/qt56/bin/qt56-env.sh
-mkdir build
-cd build
-cmake .. -DWITH_COVERAGE=ON
-make -j4 
-ctest -V
