e4c50929299e4ff47fe908e6a43640c44c147a24
Remigiusz Kołłątaj <remigiusz.kollataj@gmail.com>
2017-07-14 20:16:55 +0200

    Update generateDocumentationAndDeploy.sh

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -74,7 +74,7 @@ doxygen $DOXYFILE 2>&1 | tee html/doxygen.log
 # Check this by verifying that the html directory and the file html/index.html
 # both exist. This is a good indication that Doxygen did it's work.
 if [ -d "html" ] && [ -f "html/index.html" ]; then
-
+    cd html
     echo 'Uploading documentation to the gh-pages branch...'
     # Add everything in this directory (the Doxygen code documentation) to the
     # gh-pages branch.
