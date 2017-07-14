6145697605a9ad6ee21f7af4a38100bfc9188f90
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2017-07-14 16:02:25 +0200

    provide full path to doxygen bin
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -71,7 +71,7 @@ echo "" > .nojekyll
 ##### Generate the Doxygen code documentation and log the output.          #####
 echo 'Generating Doxygen code documentation...'
 # Redirect both stderr and stdout to the log file AND the console.
-doxygen $DOXYFILE 2>&1 | tee doxygen.log
+/usr/bin/doxygen $DOXYFILE 2>&1 | tee doxygen.log
 
 ################################################################################
 ##### Upload the documentation to the gh-pages branch of the repository.   #####
