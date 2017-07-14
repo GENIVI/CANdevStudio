5708efe5bbb26c6d0097b98e5882726640f6c235
Remigiusz Kołłątaj <remigiusz.kollataj@gmail.com>
2017-07-14 22:27:46 +0200

    Clear page
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@gmail.com>

--- class_can_signal_sender.js
+++
@@ -1,6 +0,0 @@
-var class_can_signal_sender =
-[
-    [ "CanSignalSender", "class_can_signal_sender.html#a588d709cbd694cee5eb446af7f1e8c29", null ],
-    [ "~CanSignalSender", "class_can_signal_sender.html#a26dc4f8d371d131373d472d23d1256ba", null ],
-    [ "sendSignal", "class_can_signal_sender.html#aa6e8810d2c6ff445d5e9ff0ec2d33be2", null ]
-];
--- search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['candevice',['canDevice',['../class_can_device_private.html#a22c3020712af344e4326866e5b17614d',1,'CanDevicePrivate']]]
-];
