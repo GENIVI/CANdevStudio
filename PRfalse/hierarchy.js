93a8fd1140993e98f1ecb4b86615552e66dc7fcb
Travis CI <travis@travis-ci.org>
2017-07-15 13:10:24 +0000

    Deploy code docs to GitHub Pages Travis build: 100
    Commit: 32547088632a762022b10735455ca420f1046fbb

---
+++ PRfalse/hierarchy.js
@@ -0,0 +1,26 @@
+var hierarchy =
+[
+    [ "CanDeviceInterface", "struct_can_device_interface.html", [
+      [ "CanDeviceQt", "struct_can_device_qt.html", null ]
+    ] ],
+    [ "CanDevicePrivate", "class_can_device_private.html", null ],
+    [ "CanFactoryInterface", "struct_can_factory_interface.html", [
+      [ "CanFactoryQt", "struct_can_factory_qt.html", null ]
+    ] ],
+    [ "QMainWindow", null, [
+      [ "MainWindow", "class_main_window.html", null ]
+    ] ],
+    [ "QObject", null, [
+      [ "CanDevice", "class_can_device.html", null ],
+      [ "CanRawSenderPrivate", "class_can_raw_sender_private.html", null ],
+      [ "CanRawViewPrivate", "class_can_raw_view_private.html", null ],
+      [ "CanSignalSenderPrivate", "class_can_signal_sender_private.html", null ],
+      [ "CanSignalViewPrivate", "class_can_signal_view_private.html", null ]
+    ] ],
+    [ "QWidget", null, [
+      [ "CanRawSender", "class_can_raw_sender.html", null ],
+      [ "CanRawView", "class_can_raw_view.html", null ],
+      [ "CanSignalSender", "class_can_signal_sender.html", null ],
+      [ "CanSignalView", "class_can_signal_view.html", null ]
+    ] ]
+];
