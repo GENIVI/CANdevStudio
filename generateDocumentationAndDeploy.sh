b3793f3f46037bc6bc6ffb5ff32bfb948fec36d7
Remigiusz Kołłątaj <remigiusz.kollataj@gmail.com>
2017-07-15 15:15:51 +0200

    change sh to bash
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@gmail.com>

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ################################################################################
 # Title         : generateDocumentationAndDeploy.sh
 # Date created  : 2016/02/22
