1a613c3db6896289bec6ee7756dc2443230962a5
grzszc <grzegorz.szczurek@mobica.com>
2017-07-21 09:43:12 +0200

    CDS-35 CANrawViewer Added possibility to stop showing the can frames (#28)
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- src/components/canrawview/canrawview.cpp
+++ src/components/canrawview/canrawview.cpp
@@ -1,6 +1,7 @@
 #include "canrawview.h"
 #include "canrawview_p.h"
 #include <QtCore/QList>
+#include <QtCore/QString>
 #include <QtCore/QStringList>
 #include <QtGui/QStandardItem>
 #include <QtSerialBus/QCanBusFrame>
@@ -14,46 +15,36 @@ CanRawView::CanRawView(QWidget* parent)
     setLayout(d->ui->layout);
 }
 
-CanRawView::~CanRawView()
-{
-}
+CanRawView::~CanRawView() {}
 
-void CanRawView::frameReceived(const QCanBusFrame& frame)
+void CanRawView::frameView(const QCanBusFrame& frame, const QString& direction)
 {
     Q_D(CanRawView);
 
     auto payHex = frame.payload().toHex();
-    for (int i = payHex.size(); i >= 2; i -= 2) {
-        payHex.insert(i, ' ');
+    for (int ii = payHex.size(); ii >= 2; ii -= 2) {
+        payHex.insert(ii, ' ');
     }
 
     QList<QStandardItem*> list;
     list.append(new QStandardItem("0"));
     list.append(new QStandardItem("0x" + QString::number(frame.frameId(), 16)));
-    list.append(new QStandardItem("RX"));
+    list.append(new QStandardItem(direction));
     list.append(new QStandardItem(QString::number(frame.payload().size())));
     list.append(new QStandardItem(QString::fromUtf8(payHex.data(), payHex.size())));
 
     d->tvModel.appendRow(list);
+
+    if (d->ui->freezeBox->isChecked() == false) {
+        d->ui->tv->scrollToBottom();
+    }
 }
+
+void CanRawView::frameReceived(const QCanBusFrame& frame) { frameView(frame, "RX"); }
 
 void CanRawView::frameSent(bool status, const QCanBusFrame& frame, const QVariant&)
 {
-    Q_D(CanRawView);
-
     if (status) {
-        auto payHex = frame.payload().toHex();
-        for (int i = payHex.size(); i >= 2; i -= 2) {
-            payHex.insert(i, ' ');
-        }
-
-        QList<QStandardItem*> list;
-        list.append(new QStandardItem("0"));
-        list.append(new QStandardItem("0x" + QString::number(frame.frameId(), 16)));
-        list.append(new QStandardItem("TX"));
-        list.append(new QStandardItem(QString::number(frame.payload().size())));
-        list.append(new QStandardItem(QString::fromUtf8(payHex.data(), payHex.size())));
-
-        d->tvModel.appendRow(list);
+        frameView(frame, "TX");
     }
 }
