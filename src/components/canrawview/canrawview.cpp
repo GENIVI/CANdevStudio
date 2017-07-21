e9ebe4ca6e4f8d84c0a90236d3248708d6820780
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2017-07-21 09:05:45 +0200

    Clang-Format applied
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- src/components/canrawview/canrawview.cpp
+++ src/components/canrawview/canrawview.cpp
@@ -1,8 +1,8 @@
 #include "canrawview.h"
 #include "canrawview_p.h"
 #include <QtCore/QList>
-#include <QtCore/QStringList>
 #include <QtCore/QString>
+#include <QtCore/QStringList>
 #include <QtGui/QStandardItem>
 #include <QtSerialBus/QCanBusFrame>
 
@@ -15,38 +15,32 @@ CanRawView::CanRawView(QWidget* parent)
     setLayout(d->ui->layout);
 }
 
-CanRawView::~CanRawView()
-{
-}
+CanRawView::~CanRawView() {}
 
 void CanRawView::frameView(const QCanBusFrame& frame, const QString& direction)
 {
-   Q_D(CanRawView);
+    Q_D(CanRawView);
 
-   auto payHex = frame.payload().toHex();
-   for (int ii = payHex.size(); ii >= 2; ii -= 2) {
-       payHex.insert(ii, ' ');
-   }
+    auto payHex = frame.payload().toHex();
+    for (int ii = payHex.size(); ii >= 2; ii -= 2) {
+        payHex.insert(ii, ' ');
+    }
 
-   QList<QStandardItem*> list;
-   list.append(new QStandardItem("0"));
-   list.append(new QStandardItem("0x" + QString::number(frame.frameId(), 16)));
-   list.append(new QStandardItem(direction));
-   list.append(new QStandardItem(QString::number(frame.payload().size())));
-   list.append(new QStandardItem(QString::fromUtf8(payHex.data(), payHex.size())));
+    QList<QStandardItem*> list;
+    list.append(new QStandardItem("0"));
+    list.append(new QStandardItem("0x" + QString::number(frame.frameId(), 16)));
+    list.append(new QStandardItem(direction));
+    list.append(new QStandardItem(QString::number(frame.payload().size())));
+    list.append(new QStandardItem(QString::fromUtf8(payHex.data(), payHex.size())));
 
-   d->tvModel.appendRow(list);
+    d->tvModel.appendRow(list);
 
-   if (d->ui->freezeBox->isChecked() == false) {
-       d->ui->tv->scrollToBottom();
-   }
-
+    if (d->ui->freezeBox->isChecked() == false) {
+        d->ui->tv->scrollToBottom();
+    }
 }
 
-void CanRawView::frameReceived(const QCanBusFrame& frame)
-{
-    frameView(frame, "RX");
-}
+void CanRawView::frameReceived(const QCanBusFrame& frame) { frameView(frame, "RX"); }
 
 void CanRawView::frameSent(bool status, const QCanBusFrame& frame, const QVariant&)
 {
