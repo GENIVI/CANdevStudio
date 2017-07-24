cffb9831aaaaee55c3c0fc77843fa11749708e76
Grzegorz Szczurek <grzegorz.szczurek@mobica.com>
2017-07-24 11:59:25 +0200

    Added checkbox row. In future checkbox adds possibility to send frame periodically

--- src/components/canrawsender/canrawsender_p.h
+++ src/components/canrawsender/canrawsender_p.h
@@ -5,6 +5,9 @@
 #include <memory>
 #include <QtSerialBus/QCanBusFrame>
 #include <QtGui/QStandardItemModel>
+#include <QCheckBox>
+#include <QStandardItemModel>
+#include <QLineEdit>
 
 namespace Ui {
 class CanRawSenderPrivate;
@@ -21,7 +24,7 @@ public:
     {
         ui->setupUi(this);
 
-        tvModel.setHorizontalHeaderLabels({"ID", "Data", "Cyclic", ""});
+        tvModel.setHorizontalHeaderLabels({"ID", "Data", "Cyclic", "Loop", ""});
         ui->tv->setModel(&tvModel);
         ui->tv->horizontalHeader()->setSectionsMovable(true);
         ui->tv->setSelectionBehavior(QAbstractItemView::SelectRows);
@@ -62,12 +65,39 @@ private slots:
     {
         QStandardItem* id = new QStandardItem();
         QStandardItem* data = new QStandardItem();
-        QStandardItem* cyclic = new QStandardItem("0");
-        QList<QStandardItem*> list {id, data, cyclic};
+
+        QList<QStandardItem*> list {id, data};
         tvModel.appendRow(list);
 
+        QLineEdit* cyclic = new QLineEdit;
+        cyclic->setFrame(false);
+        cyclic->setAlignment(Qt::AlignHCenter);
+        cyclic->setPlaceholderText("Time in ms");
+        QRegExp rx("[1-9]\\d{0,6}");
+        QValidator *v = new QRegExpValidator(rx, this);
+        cyclic->setValidator(v);
+        ui->tv->setIndexWidget(tvModel.index(tvModel.rowCount() - 1, tvModel.columnCount() -3), cyclic);
+
+        QWidget *loopWidget = new QWidget();
+        QCheckBox *loopCheckBox = new QCheckBox();
+        loopCheckBox->setCheckable(false);
+        QHBoxLayout *loopLayout = new QHBoxLayout(loopWidget);
+        loopLayout->addWidget(loopCheckBox);
+        loopLayout->setAlignment(Qt::AlignCenter);
+        loopLayout->setContentsMargins(0,0,0,0);
+        loopWidget->setLayout(loopLayout);
+        ui->tv->setIndexWidget(tvModel.index(tvModel.rowCount() - 1, tvModel.columnCount() -2), loopWidget);
+
+        connect(cyclic, &QLineEdit::textChanged, this, [this, cyclic, loopCheckBox]{
+            if(cyclic->text().length() > 0)
+                loopCheckBox->setCheckable(true);
+            else
+                loopCheckBox->setCheckable(false);
+        });
+
         QPushButton* pbSend = new QPushButton("Send");
-        ui->tv->setIndexWidget(tvModel.index(tvModel.rowCount() - 1, 3), pbSend);
+        ui->tv->setIndexWidget(tvModel.index(tvModel.rowCount() - 1, tvModel.columnCount() -1), pbSend);
+
         connect(pbSend, &QPushButton::pressed, this, [this, id, data, cyclic] {
             Q_Q(CanRawSender);
 
