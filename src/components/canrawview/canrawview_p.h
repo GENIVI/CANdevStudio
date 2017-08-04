4e768d25c8db1a8f7d6d40248fa27bbd70e15d99
Adam Paprocki <woeflele@gmail.com>
2017-08-04 10:10:45 +0200

    CDS-33 Drag/Drop columns / CDS-34 Sort columns (#34)
    * QTableView layout properties have been move to ui file. Sorting and resizing to content is implemented.
    Added connection and slot update() which is supposed to update sort order and column sizes on model data change, but it will be implemented in another task.
    
    * Add setSectinsMovable(true);
    
    * Implemented 3-state sort feature

--- src/components/canrawview/canrawview_p.h
+++ src/components/canrawview/canrawview_p.h
@@ -7,6 +7,8 @@
 #include <QtGui/QStandardItemModel>
 #include <QtSerialBus/QCanBusFrame>
 #include <memory>
+#include <iostream>
+#include <QHeaderView>
 
 namespace Ui {
 class CanRawViewPrivate;
@@ -27,16 +29,17 @@ public:
     {
         ui->setupUi(this);
 
-        tvModel.setHorizontalHeaderLabels({ "time", "id", "dir", "dlc", "data" });
+        tvModel.setHorizontalHeaderLabels({ "rowID", "time", "id", "dir", "dlc", "data" });
         ui->tv->setModel(&tvModel);
-        ui->tv->setColumnWidth(0, 36);
-        ui->tv->setColumnWidth(1, 92);
-        ui->tv->setColumnWidth(2, 27);
-        ui->tv->setColumnWidth(3, 25);
-        ui->tv->setColumnWidth(4, 178);
+        ui->tv->horizontalHeader()->setSectionsMovable(true);
+        //ui->tv->setColumnHidden(0, true);
 
         connect(ui->pbClear, &QPushButton::pressed, this, &CanRawViewPrivate::clear);
         connect(ui->pbDockUndock, &QPushButton::pressed, this, &CanRawViewPrivate::dockUndock);
+
+        connect(ui->tv->horizontalHeader(), &QHeaderView::sectionClicked, [=]( const int &logicalIndex ) { sort( logicalIndex, clickedIndexes ); });
+        connect(&tvModel, &QAbstractItemModel::rowsInserted, [=]() { update( clickedIndexes ); });
+
     }
 
     ~CanRawViewPrivate() {}
@@ -52,8 +55,10 @@ public:
         for (int ii = payHex.size(); ii >= 2; ii -= 2) {
             payHex.insert(ii, ' ');
         }
-
+        
+        static int rowID = 0;
         QList<QStandardItem*> list;
+        list.append(new QStandardItem(QString::number(rowID++)));
         list.append(new QStandardItem(QString::number((double)timer->elapsed() / 1000, 'f', 2)));
         list.append(new QStandardItem("0x" + QString::number(frame.frameId(), 16)));
         list.append(new QStandardItem(direction));
@@ -71,6 +76,7 @@ public:
     std::unique_ptr<QElapsedTimer> timer;
     QStandardItemModel tvModel;
     bool simStarted;
+    QList<int> clickedIndexes = {0,0,0};
 
 private:
     CanRawView* q_ptr;
@@ -82,6 +88,32 @@ private slots:
     {
         Q_Q(CanRawView);
         emit q->dockUndock();
+    }
+
+    void update(QList<int> &clickedIndexes)
+    {
+    }
+
+    void sort(const int currentIndex, QList<int> &clickedIndexes)
+    {
+        clickedIndexes.removeFirst();
+        clickedIndexes.append(currentIndex);
+
+        if ((clickedIndexes[2] == clickedIndexes[1]) && (clickedIndexes[1] == clickedIndexes[0]))
+        {
+            ui->tv->sortByColumn(0,Qt::AscendingOrder);
+            clickedIndexes = {0,0,0};
+        }
+        else if (clickedIndexes[2] == clickedIndexes[1])
+        {
+            ui->tv->sortByColumn(currentIndex,Qt::DescendingOrder);
+            ui->tv->horizontalHeader()->setSortIndicator(currentIndex,Qt::DescendingOrder);
+        }
+        else
+        {
+            ui->tv->sortByColumn(currentIndex,Qt::AscendingOrder);
+            ui->tv->horizontalHeader()->setSortIndicator(currentIndex,Qt::AscendingOrder);
+        }
     }
 };
 #endif // CANRAWVIEW_P_H
