7c797c3d97d23dd589da941fab6dce8b53221e1a
Travis CI <travis@travis-ci.org>
2017-08-04 08:17:59 +0000

    Deploy code docs to GitHub Pages Travis build: 158
    Commit: 4e768d25c8db1a8f7d6d40248fa27bbd70e15d99

--- master/class_can_raw_view_private.js
+++ master/class_can_raw_view_private.js
@@ -3,6 +3,7 @@ var class_can_raw_view_private =
     [ "CanRawViewPrivate", "class_can_raw_view_private.html#ac5c02a4d9be2ca08b1f33dcc2e111df6", null ],
     [ "~CanRawViewPrivate", "class_can_raw_view_private.html#a2457b43c2a434f6c6827ded804f43d0d", null ],
     [ "frameView", "class_can_raw_view_private.html#a1573036ba16b36b7c6ce61cacf6124a5", null ],
+    [ "clickedIndexes", "class_can_raw_view_private.html#a0f3944d5b50d0013fed9b1af0576bf44", null ],
     [ "simStarted", "class_can_raw_view_private.html#a1773e4feac7c9c06eeb7076855ca5fd8", null ],
     [ "timer", "class_can_raw_view_private.html#a0d9238e0c786981ae30c5564ca4edcf0", null ],
     [ "tvModel", "class_can_raw_view_private.html#acc80cef35de6b73126143cb55708db59", null ],
--- master/search/variables_0.js
+++ master/search/variables_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['candevice',['canDevice',['../class_can_device_private.html#a22c3020712af344e4326866e5b17614d',1,'CanDevicePrivate']]]
+  ['candevice',['canDevice',['../class_can_device_private.html#a22c3020712af344e4326866e5b17614d',1,'CanDevicePrivate']]],
+  ['clickedindexes',['clickedIndexes',['../class_can_raw_view_private.html#a0f3944d5b50d0013fed9b1af0576bf44',1,'CanRawViewPrivate']]]
 ];
