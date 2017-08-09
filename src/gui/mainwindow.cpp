dd315d09a2ee1181efd821c7fd226b5d3e8bd822
Przemysław Draszkiewicz <przemyslaw.draszkiewicz@gmail.com>
2017-08-09 11:59:19 +0200

    CDS-64 Integration of nodeeditor library (#45)

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -1,10 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
-#include "candevice/candevice.h"
-#include "canrawsender/canrawsender.h"
-#include "canrawview/canrawview.h"
 #include "mainwindow.h"
+
 #include <QtWidgets/QMdiArea>
 #include <QtWidgets/QMdiSubWindow>
 #include <QtWidgets/QMessageBox>
@@ -17,48 +14,20 @@ MainWindow::MainWindow(QWidget* parent)
     ui->centralWidget->layout()->setContentsMargins(0, 0, 0, 0);
 
     CanFactoryQt factory;
-    CanDevice* canDevice = new CanDevice(factory);
-    CanRawView* canRawView = new CanRawView();
-    CanRawSender* canRawSender = new CanRawSender();
+    auto modelRegistry = std::make_shared<QtNodes::DataModelRegistry>();
 
-    canRawView->setWindowTitle("Can Raw View");
-    ui->mdiArea->addSubWindow(canRawView);
+    canDevice = std::make_shared<CanDevice>(factory);
+    graphScene = std::make_shared<QtNodes::FlowScene>(modelRegistry);
+    canRawView = new CanRawView();
+    canRawSender = new CanRawSender();
+    graphView = new QtNodes::FlowView(graphScene.get());
 
-    canRawSender->setWindowTitle("Can Raw Sender");
-    ui->mdiArea->addSubWindow(canRawSender);
+    setupMdiArea();
+    connectCanComponentsSignals();
+    connectToolbarSignals();
+    connectMenuSignals();
+    connectDockingSignals();
 
-    ui->mdiArea->tileSubWindows();
-
-    connect(canDevice, &CanDevice::frameReceived, canRawView, &CanRawView::frameReceived);
-    connect(canDevice, &CanDevice::frameSent, canRawView, &CanRawView::frameSent);
-    connect(ui->actionstart, &QAction::triggered, canRawView, &CanRawView::startSimulation);
-    connect(ui->actionstop, &QAction::triggered, canRawView, &CanRawView::stopSimulation);
-    connect(canRawSender, &CanRawSender::sendFrame, canDevice, &CanDevice::sendFrame);
-
-    connect(ui->actionstart, &QAction::triggered, ui->actionstop, &QAction::setDisabled);
-    connect(ui->actionstart, &QAction::triggered, ui->actionstart, &QAction::setEnabled);
-    connect(ui->actionstop, &QAction::triggered, ui->actionstop, &QAction::setEnabled);
-    connect(ui->actionstop, &QAction::triggered, ui->actionstart, &QAction::setDisabled);
-
-    QActionGroup *ViewModes = new QActionGroup(this);
-    ViewModes->addAction(ui->actionTabView);
-    ViewModes->addAction(ui->actionSubWindowView);
-    connect(ui->actionAbout,&QAction::triggered,this,[this] { QMessageBox::about(this,"About","<about body>"); });
-    connect(ui->actionExit,&QAction::triggered,this,&MainWindow::handleExitAction);
-    connect(ui->actionTile,&QAction::triggered,ui->mdiArea,&QMdiArea::tileSubWindows);
-    connect(ui->actionCascade,&QAction::triggered,ui->mdiArea,&QMdiArea::cascadeSubWindows);
-    connect(ui->actionTabView,&QAction::triggered,this,[this]{ ui->mdiArea->setViewMode(QMdiArea::TabbedView); });
-    connect(ui->actionTabView,&QAction::toggled,ui->actionTile,&QAction::setDisabled);
-    connect(ui->actionTabView,&QAction::toggled,ui->actionCascade,&QAction::setDisabled);
-    connect(ui->actionSubWindowView,&QAction::triggered,this,[this]{ ui->mdiArea->setViewMode(QMdiArea::SubWindowView); });
-
-    //docking signals connection
-    connect(canRawView, &CanRawView::dockUndock, this, [this, canRawView] {
-                        handleDock(canRawView, ui->mdiArea);
-		    });
-    connect(canRawSender, &CanRawSender::dockUndock, this, [this, canRawSender] {
-			handleDock(canRawSender, ui->mdiArea);
-		    });
     canDevice->init("socketcan", "can0");
     canDevice->start();
 }
@@ -94,4 +63,61 @@ void MainWindow::handleExitAction()
     if(userReply == QMessageBox::Yes)
         QApplication::quit();
 
+}
+
+void MainWindow::connectCanComponentsSignals()
+{
+    connect(canDevice.get(), &CanDevice::frameReceived, canRawView, &CanRawView::frameReceived);
+    connect(canDevice.get(), &CanDevice::frameSent, canRawView, &CanRawView::frameSent);
+    connect(ui->actionstart, &QAction::triggered, canRawView, &CanRawView::startSimulation);
+    connect(ui->actionstop, &QAction::triggered, canRawView, &CanRawView::stopSimulation);
+    connect(canRawSender, &CanRawSender::sendFrame, canDevice.get(), &CanDevice::sendFrame);
+}
+
+void MainWindow::connectToolbarSignals()
+{
+    connect(ui->actionstart, &QAction::triggered, ui->actionstop, &QAction::setDisabled);
+    connect(ui->actionstart, &QAction::triggered, ui->actionstart, &QAction::setEnabled);
+    connect(ui->actionstop, &QAction::triggered, ui->actionstop, &QAction::setEnabled);
+    connect(ui->actionstop, &QAction::triggered, ui->actionstart, &QAction::setDisabled);
+}
+
+void MainWindow::connectMenuSignals()
+{
+    QActionGroup *ViewModes = new QActionGroup(this);
+    ViewModes->addAction(ui->actionTabView);
+    ViewModes->addAction(ui->actionSubWindowView);
+    connect(ui->actionAbout,&QAction::triggered,this,[this] { QMessageBox::about(this,"About","<about body>"); });
+    connect(ui->actionExit,&QAction::triggered,this,&MainWindow::handleExitAction);
+    connect(ui->actionTile,&QAction::triggered,ui->mdiArea,&QMdiArea::tileSubWindows);
+    connect(ui->actionCascade,&QAction::triggered,ui->mdiArea,&QMdiArea::cascadeSubWindows);
+    connect(ui->actionTabView,&QAction::triggered,this,[this]{ ui->mdiArea->setViewMode(QMdiArea::TabbedView); });
+    connect(ui->actionTabView,&QAction::toggled,ui->actionTile,&QAction::setDisabled);
+    connect(ui->actionTabView,&QAction::toggled,ui->actionCascade,&QAction::setDisabled);
+    connect(ui->actionSubWindowView,&QAction::triggered,this,[this]{ ui->mdiArea->setViewMode(QMdiArea::SubWindowView); });
+}
+
+void MainWindow::connectDockingSignals()
+{
+    connect(canRawView, &CanRawView::dockUndock, this, [this] {
+		                        handleDock(canRawView, ui->mdiArea);
+					                    });
+
+    connect(canRawSender, &CanRawSender::dockUndock, this, [this] {
+		                        handleDock(canRawSender, ui->mdiArea);
+					                    });
+}
+
+void MainWindow::setupMdiArea()
+{
+    canRawView->setWindowTitle("Can Raw View");
+    ui->mdiArea->addSubWindow(canRawView);
+
+    canRawSender->setWindowTitle("Can Raw Sender");
+    ui->mdiArea->addSubWindow(canRawSender);
+
+    graphView->setWindowTitle("Graph");
+    ui->mdiArea->addSubWindow(graphView);
+
+    ui->mdiArea->tileSubWindows();
 }
--- src/gui/mainwindow.h
+++ src/gui/mainwindow.h
@@ -5,6 +5,13 @@
 #include <memory>
 
 #include "candevice/canfactoryqt.hpp"
+#include "candevice/candevice.h"
+#include "canrawsender/canrawsender.h"
+#include "canrawview/canrawview.h"
+
+#include <nodes/NodeData>
+#include <nodes/FlowScene>
+#include <nodes/FlowView>
 
 class QMdiArea;
 
@@ -22,7 +29,19 @@ public:
 
 private:
     std::unique_ptr<Ui::MainWindow> ui;
+    std::shared_ptr<CanDevice> canDevice;
+    std::shared_ptr<QtNodes::FlowScene> graphScene;
+    CanRawView* canRawView;
+    CanRawSender* canRawSender;
+    QtNodes::FlowView* graphView;
+
     void handleDock(QWidget* component, QMdiArea* mdi);
+    void connectCanComponentsSignals();
+    void connectToolbarSignals();
+    void connectMenuSignals();
+    void connectDockingSignals();
+    void setupMdiArea();
+
 private slots:
     void handleExitAction(void);
 };
