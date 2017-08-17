2cd906af1126fd0e3f9852510bfbe25db2f05842
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2017-08-17 09:05:45 +0200

    Fix CanFactory object lifetime. Add default factory
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- src/components/candevice/candevice_p.h
+++ src/components/candevice/candevice_p.h
@@ -1,21 +1,36 @@
 #ifndef __CANDEVICE_P_H
 #define __CANDEVICE_P_H
 
+#include "candeviceinterface.hpp"
+#include "canfactoryqt.hpp"
 #include <QtCore/QObject>
 #include <QtCore/QScopedPointer>
 #include <QtCore/QVariant>
 #include <QtCore/QVector>
 #include <QtSerialBus/QCanBus>
 #include <QtSerialBus/QCanBusFrame>
-#include "candeviceinterface.hpp"
 #include <memory>
 
 class CanDevicePrivate {
 public:
+    CanDevicePrivate()
+        : mFactory(mDefFactory)
+    {
+    }
+
+    CanDevicePrivate(CanFactoryInterface& factory)
+        : mFactory(factory)
+    {
+    }
+
     QString mBackend;
     QString mInterface;
     QVector<QCanBusFrame> mSendQueue;
     std::unique_ptr<CanDeviceInterface> canDevice;
+    CanFactoryInterface& mFactory;
+
+private:
+    CanFactoryQt mDefFactory;
 };
 
 #endif /* !__CANDEVICE_P_H */
