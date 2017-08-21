f7b9e06f4432b683cd3c33d852e5aa7aa32f21b3
Krzysztof Ostrowski <krzysztof.ostrowski@mobica.com>
2017-08-21 13:38:41 +0200

    Preview of model visitor.

---
+++ src/common/modelvisitor.h
@@ -0,0 +1,74 @@
+
+#ifndef MODELVISITOR_H
+#define MODELVISITOR_H
+
+#include <type_traits> // is_same, result_of
+#include <utility> // move
+
+#include <nodes/NodeDataModel>
+
+#include "visitablewith.h" // VisitableWith
+
+
+class CanRawViewModel;
+class CanRawSenderModel;
+
+/**
+ * Example usage with @c VisitableWith<T>:
+ *
+ * @code
+ *  auto m = node.nodeDataModel();
+ *
+ *  auto v = make_model_visitor([this](CanRawViewModel& a) { }
+ *                            , [this](CanRawSenderModel& a) { });
+ *
+ *  assert(nullptr != m);
+ *
+ *  apply_model_visitor(*m, std::move(v));
+ *  // or apply_model_visitor(*m, std::move(std::ref(v)));
+ * @endcode
+ */
+template<
+    class CanRawViewModelAction
+  , class CanRawSenderModelAction
+  >
+struct NodeDataModelVisitor
+{
+    void operator()(CanRawViewModel& a)   { _f(a); }
+    void operator()(CanRawSenderModel& a) { _g(a); }
+
+    CanRawViewModelAction   _f;
+    CanRawSenderModelAction _g;
+
+    NodeDataModelVisitor(CanRawViewModelAction f, CanRawSenderModelAction g)
+      : _f(std::move(f)), _g(std::move(g))
+    {}
+};
+
+// TODO: C++17: use deduction rules
+template<
+    class CanRawViewModelAction
+  , class CanRawSenderModelAction
+  >
+inline NodeDataModelVisitor make_model_visitor(F f, G g)
+{
+    static_assert(std::is_same<std::result_of_t<F(CanRawViewModel&)>, void>::value
+               && std::is_same<std::result_of_t<G(CanRawSenderModel&)>, void>::value
+        , "Wrong parameters");
+
+    return NodeDataModelVisitor<F, G>{std::move(f), std::move(g)};
+}
+
+inline apply_model_visitor(NodeDataModel& m, NodeDataModelVisitor v)
+{
+    using B = VisitableWith<NodeDataModelVisitor>;
+
+#ifndef DEBUG
+    static_cast<B&>(m).visit(v);
+#else
+    dynamic_cast<B&>(m).visit(v);  // throws!
+#endif
+}
+
+#endif
+
---
+++ src/common/visitablewith.h
@@ -0,0 +1,54 @@
+
+#ifndef VISITABLEWITH_H
+#define VISITABLEWITH_H
+
+/**
+ * Example for two types that can be visited: A, B and visitor V.
+ *
+ * @code
+ *  struct V
+ *  {
+ *   void operator()(A&) { std::cout << "A\n"; }
+ *   void operator()(B&) { std::cout << "B\n"; }
+ *  };
+ *
+ *  struct A : VisitableWith<V>
+ *  {
+ *   void visit(V& v) override { v(*this); }
+ *  };
+ *
+ *  struct B : VisitableWith<V>
+ *  {
+ *   void visit(V& v) override { v(*this); }
+ *  };
+ *
+ *  int main()
+ *  {
+ *   A a;
+ *   B b;
+ *   V v;
+ *
+ *   using W = VisitableWith<V>;
+ *
+ *   W& va = a;
+ *   W& vb = b;
+ *
+ *   va.visit(v);
+ *   vb.visit(v);
+ *  }
+ * @endcode
+ *
+ * gives
+ *
+ * A
+ * B
+ *
+ */
+template<class Visitor>
+struct VisitableWith
+{
+    virtual void visit(Visitor& visitor) = 0;
+};
+
+#endif
+
