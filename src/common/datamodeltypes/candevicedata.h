9ebcca0b206d57fd07f59b45275eef0375904a82
Przemysław Draszkiewicz <przemyslaw.draszkiewicz@gmail.com>
2017-08-23 10:09:26 +0200

    CDS-69 Doxygen (#56)

--- src/common/datamodeltypes/candevicedata.h
+++ src/common/datamodeltypes/candevicedata.h
@@ -7,8 +7,14 @@
 
 using QtNodes::NodeDataType;
 
+/**
+*   @brief The enum class describing frame direction
+*/
 enum class Direction { RX, TX };
 
+/**
+*   @brief The class describing data model used as input for CanDevice node
+*/
 class CanDeviceDataIn : public NodeData {
 public:
     CanDeviceDataIn(){};
@@ -16,15 +22,23 @@ public:
         : _frame(frame)
     {
     }
-
+    /**
+    *   @brief  Used to get data type id and displayed text for ports
+    *   @return NodeDataType of rawsender
+    */
     NodeDataType type() const override { return NodeDataType{ "rawsender", "Raw" }; }
-
+    /**
+    *   @brief  Used to get frame
+    */
     QCanBusFrame frame() const { return _frame; };
 
 private:
     QCanBusFrame _frame;
 };
 
+/**
+*   @brief The class describing data model used as output for CanDevice node
+*/
 class CanDeviceDataOut : public NodeData {
 public:
     CanDeviceDataOut(){};
@@ -34,11 +48,25 @@ public:
         , _status(status)
     {
     }
-
+    /**
+    *   @brief  Used to get data type id and displayed text for ports
+    *   @return NodeDataType of rawview
+    */
     NodeDataType type() const override { return NodeDataType{ "rawview", "Raw" }; }
 
+    /**
+    *   @brief  Used to get frame
+    */
     QCanBusFrame frame() const { return _frame; };
+
+    /**
+    *   @brief  Used to get direction
+    */
     Direction direction() const { return _direction; };
+
+    /**
+    *   @brief  Used to get status
+    */
     bool status() const { return _status; };
 
 private:
--- src/components/canrawview/canrawviewmodel.h
+++ src/components/canrawview/canrawviewmodel.h
@@ -17,6 +17,9 @@ using QtNodes::NodeDataType;
 using QtNodes::NodeDataModel;
 using QtNodes::NodeValidationState;
 
+/**
+*   @brief The class provides node graphical representation of CanRawView
+*/
 class CanRawViewModel : public NodeDataModel {
     Q_OBJECT
 
@@ -26,26 +29,68 @@ public:
     virtual ~CanRawViewModel() {}
 
 public:
+    /**
+    *   @brief  Used to get node caption
+    *   @return Node caption
+    */
     QString caption() const override { return QString("CanRawView Node"); } // TODO
 
+    /**
+    *   @brief  Used to identify model by data model name
+    *   @return Node model name
+    */
     QString name() const override { return QString("CanRawViewModel"); }
 
     std::unique_ptr<NodeDataModel> clone() const override { return std::make_unique<CanRawViewModel>(); }
 
 public:
+    /**
+    *   @brief  Used to get model name
+    *   @return Model name
+    */
     virtual QString modelName() const { return QString("Raw view"); }
 
+    /**
+    *   @brief  Used to get number of ports of each type used by model
+    *   @param  type of port
+    *   @return 1 if port in, 0 if any other type
+    */
     unsigned int nPorts(PortType portType) const override;
 
+    /**
+    *   @brief  Used to get data type of each port
+    *   @param  type of port
+    *   @patam  port id
+    *   @return CanDeviceDataOIn type if portType is out
+    */
     NodeDataType dataType(PortType portType, PortIndex portIndex) const override;
 
+    /**
+    *   @brief  Sets output data for propagation, not used in this class
+    *   @param  port id
+    *   @return
+    */
     std::shared_ptr<NodeData> outData(PortIndex port) override;
 
+    /**
+    *   @brief  Handles data on input port, send frames to CanRawView
+    *   @param  data on port
+    *   @param  port id
+    */
     void setInData(std::shared_ptr<NodeData> nodeData, PortIndex port) override;
 
+    /**
+    *   @brief  Used to get widget embedded in Node
+    *   @return QLabel
+    */
     QWidget* embeddedWidget() override { return label; }
 
+    /**
+    *   @brief  Used to get information if node is resizable
+    *   @return false
+    */
     bool resizable() const override { return false; }
+
     CanRawView canRawView;
 
 private:
