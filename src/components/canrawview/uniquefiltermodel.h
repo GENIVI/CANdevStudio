27421e91a4de4a33b338c43ce0ede3bd02f58fbd
rthrh <woeflele@gmail.com>
2017-08-29 17:00:21 +0200

    Added doxygen comments to UniqueFilterModel class

--- src/components/canrawview/uniquefiltermodel.h
+++ src/components/canrawview/uniquefiltermodel.h
@@ -3,21 +3,50 @@
 
 #include <QSortFilterProxyModel>
 
+/**
+*   @brief This class provides a filter model between source model and table view, which passes through only frames with
+*          newest unique ID and direction values
+*/
 class UniqueFilterModel : public QSortFilterProxyModel {
     Q_OBJECT
 public:
     explicit UniqueFilterModel(QObject* parent = 0);
 
+    /**
+    *   @brief  Updates filter with unique values, on reception of each frame
+    *   @param  current frame ID
+    *   @param  time elapsed since simulation start
+    *   @param  direction of frame
+    */
     void updateFilter(int frameID, double time, QString direction);
+
+    /**
+    *   @brief  Clears unique value stored in filter
+    */
     void clearFilter();
+
+    /**
+    *   @brief  Indicates whether filter is currently active
+    *   @return bool
+    */
     bool isFilterActive();
 
 protected:
+    /**
+    *   @brief  Function iterates whole data model row by row and indicates, if currently processed row should be
+    *           displayed in table view or not
+    *   @param  currently processed model row number
+    *   @param  QModelIndex pointing at source data model
+    *   @return bool
+    */
     bool filterAcceptsRow(int source_row, const QModelIndex& source_parent) const override;
 
 signals:
 
 public slots:
+    /**
+    *   @brief  Function toggles filter on/off
+    */
     void toggleFilter();
 
 private:
