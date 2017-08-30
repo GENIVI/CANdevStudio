f2f2cb24c40bbf496b1b863a5842cf024e4f06dd
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2017-08-30 15:51:47 +0200

    Fixing windows handling
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

---
+++ src/gui/subwindow.hpp
@@ -0,0 +1,23 @@
+#ifndef __SUBWINDOW_HPP
+#define __SUBWINDOW_HPP
+
+#include <QMdiSubWindow>
+#include <QPushButton>
+
+struct SubWindow : public QMdiSubWindow {
+    SubWindow(QWidget *widget) : QMdiSubWindow()
+    {
+        setWidget(widget);
+    }
+
+    virtual void closeEvent(QCloseEvent *event) override
+    {
+        cds_debug("On closing");
+
+        // Allow to close the window but prevent deletion of the widget
+        setWidget(nullptr);
+        QMdiSubWindow::closeEvent(event);
+    }
+};
+
+#endif /* !__SUBWINDOW_HPP */
