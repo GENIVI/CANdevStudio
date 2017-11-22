0571543222ee6f6be82025f1b9e7a161b16bcc04
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2017-11-22 16:09:22 +0100

    Setting about dialog to frameless
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -382,6 +382,7 @@ void MainWindow::handleAboutAction()
         QDesktopServices::openUrl({"http://www.genivi.org"});
     });
 
+    d->setWindowFlags(d->windowFlags() | Qt::FramelessWindowHint);
     d->exec();
 }
 
