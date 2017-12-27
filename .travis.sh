f0fd35c5091812826bfb4aa89127b5d6449a1993
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2017-12-27 19:48:53 +0100

    Adding docker options for vcan interface
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- .travis.sh
+++ .travis.sh
@@ -9,9 +9,9 @@ fi
 echo "CMake: $cmake_command"
 echo "ci_env: $ci_env"
 docker run \
-     $ci_env -e WITH_COVERAGE -e CC -e CXX -e DISPLAY=:10\
+     $ci_env -e WITH_COVERAGE -e CC -e CXX -e DISPLAY=:10 --privileged \
      -v `pwd`:/root/sources przemyslawdraszkiewicz/can_dev_base_image /bin/bash \
-     -c "service xvfb start && cd /root/sources && rm -rf build && mkdir -p build && cd build && source /opt/qt58/bin/qt58-env.sh &&
+     -c "ip link add name can0 type vcan && service xvfb start && cd /root/sources && rm -rf build && mkdir -p build && cd build && source /opt/qt58/bin/qt58-env.sh &&
         $cmake_command &&
         make -j5 &&
         make test &&
