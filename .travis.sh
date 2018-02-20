1d5cca34bf2d5e38067609ba322051e4435ad06e
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2018-02-20 18:10:37 +0100

    Docker updated
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- .travis.sh
+++ .travis.sh
@@ -10,9 +10,9 @@ echo "CMake: $cmake_command"
 echo "ci_env: $ci_env"
 sudo modprobe vcan
 docker run \
-     $ci_env -u 1000 -e WITH_COVERAGE -e CC -e CXX -e DISPLAY=:10 --privileged --cap-add=ALL \
-     -v `pwd`:/root/sources przemyslawdraszkiewicz/can_dev_base_image /bin/bash \
-     -c "ip link add name can0 type vcan && ip link set can0 up && ip link && service xvfb start && cd /root/sources && rm -rf build && mkdir -p build && cd build && source /opt/qt58/bin/qt58-env.sh &&
+     $ci_env -e WITH_COVERAGE -u cds -e CC -e CXX -e DISPLAY=:10 --privileged --cap-add=ALL \
+     -v `pwd`:/home/sources rkollataj/candevstudio /bin/bash \
+     -c "sudo ip link add name can0 type vcan && sudo ip link set can0 up && ip link && sudo service xvfb start && cd /home/sources && rm -rf build && mkdir -p build && cd build && source /opt/qt58/bin/qt58-env.sh &&
         $cmake_command &&
         make -j5 &&
         make test &&
