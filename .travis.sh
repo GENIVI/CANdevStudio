89efbbdffbd85f18e0fabab084fb077f6c105e1e
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2018-02-20 15:31:09 +0100

    Trying to run docker with user id
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- .travis.sh
+++ .travis.sh
@@ -10,7 +10,7 @@ echo "CMake: $cmake_command"
 echo "ci_env: $ci_env"
 sudo modprobe vcan
 docker run \
-     $ci_env -e WITH_COVERAGE -e CC -e CXX -e DISPLAY=:10 --privileged --cap-add=ALL \
+     $ci_env -u 1000 -e WITH_COVERAGE -e CC -e CXX -e DISPLAY=:10 --privileged --cap-add=ALL \
      -v `pwd`:/root/sources przemyslawdraszkiewicz/can_dev_base_image /bin/bash \
      -c "ip link add name can0 type vcan && ip link set can0 up && ip link && service xvfb start && cd /root/sources && rm -rf build && mkdir -p build && cd build && source /opt/qt58/bin/qt58-env.sh &&
         $cmake_command &&
