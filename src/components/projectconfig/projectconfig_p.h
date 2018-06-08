55ca4bbbd263694abfe9b3d6136476acd5013ced
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-06-08 10:16:07 +0200

    ScrollArea implementation in ProjectConfig ui

--- src/components/projectconfig/projectconfig_p.h
+++ src/components/projectconfig/projectconfig_p.h
@@ -57,6 +57,9 @@ public:
         _ui->setupUi(this);
         _ui->layout->addWidget(_graphView);
 
+        _ui->scrollArea->setMinimumSize(_ui->frame->width(), 0);
+        _ui->scrollArea->setMaximumSize(_ui->frame->width() + 90, 10000);
+
         connect(_ui->pbDeviceLayer, &QPushButton::toggled, _ui->deviceWidget, &QWidget::setVisible);
         _ui->pbDeviceLayer->setChecked(true);
         connect(_ui->pbRawLayer, &QPushButton::toggled, _ui->rawWidget, &QWidget::setVisible);
