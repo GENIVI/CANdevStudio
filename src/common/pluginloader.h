4d6ece65f4913c6bf6feaa19bb36968440d31480
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2018-07-19 08:31:19 +0200

    Changing std::vector to std:array
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- src/common/pluginloader.h
+++ src/common/pluginloader.h
@@ -5,17 +5,14 @@
 
 #include "plugin_type.h"
 
-template <class T, class Tuple>
-struct SectionNdx;
+template <class T, class Tuple> struct section_ndx;
 
-template <class T, class... Types>
-struct SectionNdx<T, std::tuple<T, Types...>> {
+template <class T, class... Types> struct section_ndx<T, std::tuple<T, Types...>> {
     static const std::size_t value = 0;
 };
 
-template <class T, class U, class... Types>
-struct SectionNdx<T, std::tuple<U, Types...>> {
-    static const std::size_t value = 1 + SectionNdx<T, std::tuple<Types...>>::value;
+template <class T, class U, class... Types> struct section_ndx<T, std::tuple<U, Types...>> {
+    static const std::size_t value = 1 + section_ndx<T, std::tuple<Types...>>::value;
 };
 
 template <typename T> void registerModel(QtNodes::DataModelRegistry& registry)
@@ -29,10 +26,10 @@ template <typename W, typename Z, typename... Args> void registerModel(QtNodes::
     registerModel<Z, Args...>(registry);
 }
 
-template <typename... Args> struct SectionLoader;
+template <typename Array, typename... Args> struct SectionLoader;
 
-template <typename... Args> struct SectionLoader<std::tuple<Args...>> {
-    SectionLoader(std::vector<QWidget*>& widgets, Ui::ProjectConfigPrivate& ui)
+template <typename Array, typename... Args> struct SectionLoader<Array, std::tuple<Args...>> {
+    SectionLoader(Array& widgets, Ui::ProjectConfigPrivate& ui)
         : _widgets(widgets)
     {
         initSection<Args...>(ui);
@@ -47,6 +44,10 @@ private:
 
     template <typename T> void initSection(Ui::ProjectConfigPrivate& ui)
     {
+        constexpr size_t ndx = section_ndx<T, std::tuple<Args...>>::value;
+
+        static_assert(ndx < std::tuple_size<std::remove_reference_t<decltype(_widgets)>>::value, "Index out of bounds");
+
         auto pb = new QCheckBox(T::sectionName(), ui.frame);
         pb->setLayoutDirection(Qt::RightToLeft);
         pb->setStyleSheet(QString("spacing: ") + QString::number(T::spacing()) + "px;");
@@ -60,12 +61,12 @@ private:
         wdgLayout->setSpacing(10);
         wdgLayout->setContentsMargins(0, 0, 0, 0);
         ui.verticalLayout->addWidget(wdg);
-        _widgets.push_back(wdg);
+        _widgets[ndx] = wdg;
 
         QObject::connect(pb, &QPushButton::toggled, wdg, &QWidget::setVisible);
     }
 
-    std::vector<QWidget*>& _widgets;
+    Array& _widgets;
 };
 
 template <typename S, typename... Args> struct PluginLoader {
@@ -76,7 +77,7 @@ template <typename S, typename... Args> struct PluginLoader {
 
     void initSections(Ui::ProjectConfigPrivate& ui)
     {
-        SectionLoader<S> sections(_widgets, ui);
+        SectionLoader<decltype(_widgets), S> sections(_widgets, ui);
 
         auto spacer = new QSpacerItem(17, 410, QSizePolicy::Minimum, QSizePolicy::Expanding);
         ui.verticalLayout->addItem(spacer);
@@ -101,12 +102,14 @@ template <typename S, typename... Args> struct PluginLoader {
 private:
     template <typename T> void addWidget(const QColor& bg)
     {
-        size_t ndx = SectionNdx<typename T::PluginType, S>::value;
+        constexpr size_t ndx = section_ndx<typename T::PluginType, S>::value;
 
-        if ((ndx < _widgets.size()) && _widgets[ndx]) {
+        static_assert(ndx < std::tuple_size<decltype(_widgets)>::value, "Index out of bounds");
+
+        if (_widgets[ndx]) {
             _widgets[ndx]->layout()->addWidget(new IconLabel(T::name, T::PluginType::sectionColor(), bg));
         } else {
-            cds_error("Sections initialization error");
+            cds_error("Sections not initialized");
         }
     }
 
@@ -116,7 +119,7 @@ private:
         addWidget<Z, Plugs...>(bg);
     }
 
-    std::vector<QWidget*> _widgets;
+    std::array<QWidget*, std::tuple_size<S>::value> _widgets;
 };
 
 #endif /* end of include guard: PLUGINLOADER_H_QWADUNZ7 */
