7218393e7cdcc9afd20613128548271be5585244
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2018-07-20 09:53:56 +0200

    Add DEV_BUILD=OFF to macOS
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- travis/build.sh
+++ travis/build.sh
@@ -20,9 +20,31 @@ else
     cmake --build build-osx --config Release --target install
     cd build-osx
     ctest
+
+    # DEV_BUILD=ON (master)
     cpack -G DragNDrop
+    mkdir master
+    mv *.dmg master
+
+    # Deploy (master)
+    cd master
     for f in *.dmg; do
         curl -T $f -urkollataj:$BINTRAY_API_KEY https://api.bintray.com/content/rkollataj/CANdevStudio/master/${TRAVIS_COMMIT:0:7}/$f\;override=1
     done
     curl -urkollataj:$BINTRAY_API_KEY -X POST https://api.bintray.com/content/rkollataj/CANdevStudio/master/${TRAVIS_COMMIT:0:7}/publish
+    cd ..
+
+    # DEV_BUILD=OFF (rc)
+    cmake . -DDEV_BUILD=OFF
+    cpack -G DragNDrop
+    mkdir rc
+    mv *.dmg rc
+
+    # Deploy (rc)
+    cd rc
+    for f in *.dmg; do
+        curl -T $f -urkollataj:$BINTRAY_API_KEY https://api.bintray.com/content/rkollataj/CANdevStudio/cds/rc/$f\;override=1
+    done
+    curl -urkollataj:$BINTRAY_API_KEY -X POST https://api.bintray.com/content/rkollataj/CANdevStudio/cds/rc/publish
+    cd ..
 fi
