3fa7d87dff95deae32301621cf78d371fb007314
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2018-08-17 09:44:02 +0200

    check opendbc_tests failure
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- travis/docker_compile.sh
+++ travis/docker_compile.sh
@@ -18,6 +18,7 @@ docker run \
      -c "sudo ip link add name can0 type vcan && sudo ip link set can0 up && ip link && sudo service xvfb start && sudo chown cds:cds /home/sources && cd /home/sources && rm -rf build && mkdir -p build && cd build && source /opt/qt58/bin/qt58-env.sh &&
         $cmake_command &&
         make -j5 &&
+        ls ../3rdParty/CANdb/tests/dbc/opendbc/ &&
         ./3rdParty/CANdb/tests/opendbc_tests &&
         make test &&
         if [ '$WITH_COVERAGE' == 'ON' ]; then bash <(curl -s https://codecov.io/bash) -x gcov-6 || echo 'Codecov did not collect coverage reports'; fi &&
