432d5172ac964d22ff7e1498703de6fc1fae3546
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2019-02-10 09:19:20 +0100

    Change qt version for MacOS
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -6,6 +6,6 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     echo refs/heads/$TRAVIS_BRANCH~ refs/heads/$TRAVIS_BRANCH refs/heads/$TRAVIS_BRANCH | ./.trigger_appveyor.rb || true
     export ci_env=`bash <(curl -s https://codecov.io/env)`
 else
-    brew install qt5@5.11 ninja
+    brew install qt@5.11 ninja
 fi
 
