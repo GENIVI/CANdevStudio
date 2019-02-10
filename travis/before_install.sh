b1337c9017b5945648a8c81492122f1081033048
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2019-02-10 18:58:23 +0100

    test
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -6,7 +6,6 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     echo refs/heads/$TRAVIS_BRANCH~ refs/heads/$TRAVIS_BRANCH refs/heads/$TRAVIS_BRANCH | ./.trigger_appveyor.rb || true
     export ci_env=`bash <(curl -s https://codecov.io/env)`
 else
-    echo abc
-    #brew install qt5 ninja
+    brew install qt5 ninja
 fi
 
--- travis/build.sh
+++ travis/build.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
-set -x
 set -e
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    #export CC=gcc-6 CXX=g++-6 CMAKE_BUILD_TYPE=Debug WITH_COVERAGE=ON PACKAGE=OFF
-    #./travis/docker_compile.sh
-    #export CC=gcc-53 CXX=g++-53 CMAKE_BUILD_TYPE=Release WITH_COVERAGE=OFF PACKAGE=ON
-    #./travis/docker_compile.sh
-    #export CC=gcc-53 CXX=g++-53 CMAKE_BUILD_TYPE=Debug WITH_COVERAGE=OFF PACKAGE=OFF
-    #./travis/docker_compile.sh
-    #export CC=clang-3.5 CXX=clang++-3.5 CMAKE_BUILD_TYPE=Release WITH_COVERAGE=OFF PACKAGE=OFF
-    #./travis/docker_compile.sh
+    export CC=gcc-6 CXX=g++-6 CMAKE_BUILD_TYPE=Debug WITH_COVERAGE=ON PACKAGE=OFF
+    ./travis/docker_compile.sh
+    export CC=gcc-53 CXX=g++-53 CMAKE_BUILD_TYPE=Release WITH_COVERAGE=OFF PACKAGE=ON
+    ./travis/docker_compile.sh
+    export CC=gcc-53 CXX=g++-53 CMAKE_BUILD_TYPE=Debug WITH_COVERAGE=OFF PACKAGE=OFF
+    ./travis/docker_compile.sh
+    export CC=clang-3.5 CXX=clang++-3.5 CMAKE_BUILD_TYPE=Release WITH_COVERAGE=OFF PACKAGE=OFF
+    ./travis/docker_compile.sh
     export CC=clang-3.5 CXX=clang++-3.5 CMAKE_BUILD_TYPE=Debug WITH_COVERAGE=OFF PACKAGE=OFF
-    #./travis/docker_compile.sh
+    ./travis/docker_compile.sh
 else
 
-    #chmod -R 755 "$(brew --prefix qt5)/lib/"
-    #echo "Building for OSX"
-    #cmake -H. -Bbuild-osx -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$(brew --prefix qt5)/lib/cmake" -GNinja -DWITH_TESTS=ON -DCMAKE_INSTALL_PREFIX=install
+    chmod -R 755 "$(brew --prefix qt5)/lib/"
+    echo "Building for OSX"
+    cmake -H. -Bbuild-osx -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$(brew --prefix qt5)/lib/cmake" -GNinja -DWITH_TESTS=ON -DCMAKE_INSTALL_PREFIX=install
 
-    ## First install command always fails for some reason...
-    #cmake --build build-osx --config Release
-    #cmake --build build-osx --config Release --target install || true
-    #cmake --build build-osx --config Release --target install
+    cmake --build build-osx --config Release
+    # First install command always fails for some reason...
+    cmake --build build-osx --config Release --target install || true
+    cmake --build build-osx --config Release --target install
 
-    mkdir build-osx
-    pwd
-    ls -al 
-    cd build-osx || true
-    #ctest
+    # cd causes shell script to interrupt. Ignore errors onwards
+    set +e
+    cd build-osx
+    ctest
 
     # DEV_BUILD=ON (master)
     cpack -G DragNDrop
@@ -37,12 +35,12 @@ else
     mv *.dmg master
 
     # Deploy (master)
-    cd master || true
+    cd master
     for f in *.dmg; do
         curl -T $f -urkollataj:$BINTRAY_API_KEY https://api.bintray.com/content/rkollataj/CANdevStudio/master/${TRAVIS_COMMIT:0:7}/$f\;override=1
     done
     curl -urkollataj:$BINTRAY_API_KEY -X POST https://api.bintray.com/content/rkollataj/CANdevStudio/master/${TRAVIS_COMMIT:0:7}/publish
-    cd .. || true
+    cd ..
 
     # DEV_BUILD=OFF (rc)
     cmake . -DDEV_BUILD=OFF
@@ -51,10 +49,10 @@ else
     mv *.dmg rc
 
     # Deploy (rc)
-    cd rc || true
+    cd rc
     for f in *.dmg; do
         curl -T $f -urkollataj:$BINTRAY_API_KEY https://api.bintray.com/content/rkollataj/CANdevStudio/cds/rc/$f\;override=1
     done
     curl -urkollataj:$BINTRAY_API_KEY -X POST https://api.bintray.com/content/rkollataj/CANdevStudio/cds/rc/publish
-    cd .. || true
+    cd ..
 fi
