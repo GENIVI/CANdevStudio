a636f735a0d4c0021df02d74af57df45c2c93ff3
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2019-10-22 22:29:57 +0200

    Disable Homebrew auto-update to fix Travis MacOS build
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -5,6 +5,6 @@ set -e
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     export ci_env=`bash <(curl -s https://codecov.io/env)`
 else
-    brew install qt5 ninja
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install qt5 ninja
 fi
 
