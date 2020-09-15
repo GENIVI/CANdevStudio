54094eedfa8f63c0d4f3556b1bdfbba9c86ca538
Sebastian Wieczorek <sebastian.wieczorek@mobica.com>
2020-09-15 13:39:41 +0200

    Fixing CanRawData and CanSignalModel c++ include dependencies

--- src/common/datamodeltypes/canrawdata.h
+++ src/common/datamodeltypes/canrawdata.h
@@ -1,17 +1,16 @@
 #ifndef __CANRAWDATA_H
 #define __CANRAWDATA_H
 
+#include <nodes/NodeData>
 #include <nodes/NodeDataModel>
 
 #include <QCanBusFrame>
 #include "datadirection.h"
 
-using QtNodes::NodeDataType;
-
 /**
 *   @brief The class describing data model used as output for CanDevice node
 */
-class CanRawData : public NodeData {
+class CanRawData : public QtNodes::NodeData {
 public:
     CanRawData(){};
     CanRawData(QCanBusFrame const& frame, Direction const direction = Direction::TX, bool status = true)
@@ -24,9 +23,9 @@ public:
     *   @brief  Used to get data type id and displayed text for ports
     *   @return NodeDataType of rawview
     */
-    NodeDataType type() const override
+    QtNodes::NodeDataType type() const override
     {
-        return NodeDataType{ "rawframe", "RAW" };
+        return QtNodes::NodeDataType{ "rawframe", "RAW" };
     }
 
     /**
