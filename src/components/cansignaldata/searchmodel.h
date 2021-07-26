af1bcff23c957481c5623dfd2133c4e52e974fdb
obh <obh@ivisolutionn.com>
2021-07-26 11:45:40 +0900

    add function about drag & drop(cansignaldata to cansignalsender)

--- src/components/cansignaldata/searchmodel.h
+++ src/components/cansignaldata/searchmodel.h
@@ -8,6 +8,9 @@ class SearchModel : public QSortFilterProxyModel {
 public:
     bool isFilterActive() const;
 
+    QMimeData* mimeData(const QModelIndexList& indexes) const override;
+    QStringList mimeTypes() const override;
+
 protected:
     bool filterAcceptsRow(int sourceRow, const QModelIndex& sourceParent) const override;
 
